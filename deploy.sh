2fb320fda35cf2811e5bc11d9d35ab13ed103bb1
Quentin <q@uent.in>
2021-08-15 14:45:44 -0700

    fixed color names

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 /usr/local/bin/aws s3 sync _site s3://www.nwdra.ca --profile nwdra --delete
+/usr/local/bin/aws cloudfront create-invalidation --distribution-id EE36ADZKYTDD4 --paths "/*"
 
 
